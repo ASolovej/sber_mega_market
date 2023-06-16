fe8f3482f7cf5de953fd765b97aff4d82f96f574
Alexandr Solovyov <18solovjov@gmail.com>
2023-06-16 22:35:55 +0300

    add in MainPage step setTextSearchString

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,2 +1,21 @@
-package pages;public class MainPage {
+package pages;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+public class MainPage {
+
+    public MainPage() {
+        step("Открытю сайт sbermegamarket.ru",
+                () -> {open("https://sbermegamarket.ru/");});
+    }
+
+    @Step("Ввести в строку поиска: {text}")
+    public MainPage setTextSearchString(String text) {
+        $("input[type=search]").setValue(text);
+        return this;
+    }
 }
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,2 +1,14 @@
-package tests;public class SearchTests {
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+
+@Tag("UI")
+public class SearchTests extends BaseTest {
+
+    @Test
+    public void check() {
+        new MainPage().setTextSearchString("Сахар");
+    }
 }
